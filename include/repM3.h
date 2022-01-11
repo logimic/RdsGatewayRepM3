add46c789767e8e84059899cc56ce725cc8cd5d5
Marek Belisko <marek.belisko@open-nandra.com>
2022-01-11 19:45:15 +0100

    Move access classes handling to separate header
    Signed-off-by: Marek Belisko <marek.belisko@open-nandra.com>

--- include/repM3.h
+++ include/repM3.h
@@ -902,39 +902,7 @@ class GetReportCmd : public BaseCommand {
   /************** General Commands **************/
   /**********************************************/
 #endif 
-  class GetVersion : public GetVersionCmd {
-  public:
-    class Version {
-    public:
-      int minor;
-      int major;
-      int release;
-      int variant;
-
-      rapidjson::Value encode(rapidjson::Document::AllocatorType & a) {
-        using namespace rapidjson;
-        Value val(Type::kObjectType);
-        Pointer("/minor").Set(val, minor, a);
-        Pointer("/major").Set(val, major, a);
-        Pointer("/release").Set(val, release, a);
-        Pointer("/variant").Set(val, variant, a);
-        
-        return val;
-      }
-    };
-
-    Version getVersion() {
-      GetVersionCmd::data_t version = get_version();
-      Version retval;
-      
-      retval.minor = version.fw_version_minor.data;
-      retval.major = version.fw_version_major.data;
-      retval.release = version.fw_pre_release_nr.data;
-      retval.variant = version.hw_variant.data;
-
-      return retval;
-    };
-  };
+  
 # if 0
   /**********************************************/
   /************** Settings and schedules **************/
---
+++ include/repM3_provider.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <repM3.h>
+
+namespace lgmc {
+
+class GetVersion : public GetVersionCmd {
+  public:
+    class Version {
+    public:
+      int minor;
+      int major;
+      int release;
+      int variant;
+
+      rapidjson::Value encode(rapidjson::Document::AllocatorType & a) {
+        using namespace rapidjson;
+        Value val(Type::kObjectType);
+        Pointer("/minor").Set(val, minor, a);
+        Pointer("/major").Set(val, major, a);
+        Pointer("/release").Set(val, release, a);
+        Pointer("/variant").Set(val, variant, a);
+        
+        return val;
+      }
+    };
+
+    Version getVersion() {
+      GetVersionCmd::data_t version = get_version();
+      Version retval;
+      
+      retval.minor = version.fw_version_minor.data;
+      retval.major = version.fw_version_major.data;
+      retval.release = version.fw_pre_release_nr.data;
+      retval.variant = version.hw_variant.data;
+
+      return retval;
+    };
+  };
+};
